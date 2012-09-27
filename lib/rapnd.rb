3561b23f2383312e9cabf0f725fc5d05f32e6075
sequielo <dev@sequielo.com.ar>
2012-09-27 17:28:43 -0300

    added Rapnd.send_alert

--- lib/rapnd.rb
+++ lib/rapnd.rb
@@ -18,5 +18,19 @@ module Rapnd
     block_given? ? yield(Config) : Config
   end
   alias :config :configure
+  
+  
+  # Send an alert to the device, through a queue.
+  # other_properties: badge, sound, custom_properties
+  #
+  def send_alert(device_token, queue_name, alert, other_properties=nil)
+    message = {
+      :device_token => device_token.strip.gsub(/<>/,''),
+      :alert        => alert,
+    }
+    message.merge!(other_properties) if other_properties
+    
+    self.queue(queue_name, message)
+  end
 
 end
